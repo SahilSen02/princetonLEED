7b8214fe18a50ddb8e101562d61f5bcb163daa73
Joseph Yusufov <jey2011@nyu.edu>
2023-04-02 09:04:28 -0400

    non-fluid containers

--- princetonhack/settings.py
+++ princetonhack/settings.py
@@ -29,7 +29,7 @@ except KeyError as e:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".nycacropolis.tech"]
+ALLOWED_HOSTS = [".nycacropolis.tech", ".localhost", "0.0.0.0"]
 
 
 # Application definition
